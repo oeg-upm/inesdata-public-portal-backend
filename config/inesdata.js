85150d35ff443ca3b20c4a2e41c36e37a9adbaba
ppel <ppel@gmv.com>
2024-08-01 11:41:27 +0200

    #437468 - Modified Keycloak URL

--- config/inesdata.js
+++ config/inesdata.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     tokenClientId: env('TOKEN_CLIENT_ID', 'dataspace-users'),
     tokenUsername: env('TOKEN_USERNAME', 'user-c1'),
     tokenPassword: env('TOKEN_PASSWORD', 'user-c1'),
-    keycloakBaseUrl: env('KEYCLOAK_BASE_URL', 'http://keycloak:8080'),
+    keycloakBaseUrl: env('KEYCLOAK_BASE_URL', 'http://keycloak:8080/realms/dataspace'),
     catalogBaseUrl: env('CATALOG_BASE_URL', 'http://connector-c1:19193'),
     vocabulariesBaseUrl: env('VOCABULARIES_BASE_URL', 'http://connector-c1:19196')
 })
